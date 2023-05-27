6908d0268d25884fb19109a27a9ad7c9f3bde21c
SolarWolf <bradsterstrange0@gmail.com>
2023-05-26 22:16:14 -0600

    update setup to overwrite bashrc

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyo
 make -C ble.sh install PREFIX=~/.local
 echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
 
+# update bashrc
+cd
+mv ~/.config/.bashrc ~/.bashrc
+
 # moving openvpn file to correct folder
 cd
 sudo mv ~/.config/us-slc.prod.surfshark.com_udp.ovpn /etc/openvpn/

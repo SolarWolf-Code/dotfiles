46f58811400a4cb3c5c290078ef6d4af38f9a00a
SolarWolf <bradsterstrange0@gmail.com>
2023-05-28 18:53:15 -0600

    setup.sh - add setup for r2modman flag

--- setup.sh
+++ setup.sh
@@ -48,7 +48,8 @@ systemctl --user mask xdg-desktop-portal
 sudo systemctl enable bluetooth.service 
 sudo systemctl start bluetooth.service
 
+# r2modman: add --no-sandbox flag to work
+sed -i "s|^\(Exec=.*\)$|\1 --no-sandbox|" "/usr/share/applications/r2modman.desktop"
+
 # refresh i3
 i3-msg restart
-
-
